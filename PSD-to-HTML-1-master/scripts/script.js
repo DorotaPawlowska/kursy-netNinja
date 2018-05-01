e54c8f07d2c503bcc636d09c1fb8386638530cf8
Dorota Pawłowska <dpawlowska@op.pl>
2018-05-01 14:08:24 +0200

    ep#5 selektory

--- PSD-to-HTML-1-master/scripts/script.js
+++ PSD-to-HTML-1-master/scripts/script.js
@@ -1,3 +1,8 @@
-$(document).ready( function () {
-    alert("jquery loaded");
-});
+// znacznik
+$("h3").css({border: "3px solid blue"});
+
+// klasa
+$(".wrapper").css({border: "3px solid red"})
+
+// id
+$("#clients").css({border: "3px solid yellow"})
