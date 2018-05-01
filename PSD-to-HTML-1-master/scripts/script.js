0cfeecef023ddb022fbbc76095678cf5da69425f
Dorota Pawłowska <dpawlowska@op.pl>
2018-05-01 14:53:50 +0200

    ep#6 filtry

--- PSD-to-HTML-1-master/scripts/script.js
+++ PSD-to-HTML-1-master/scripts/script.js
@@ -1,8 +1,42 @@
+
+
+
+/*
+//ep#6 - filtry
+
+// $("header nav li:first").css({border: "2px solid red"});
+// $("header nav li:last").css({border: "2px solid red"});
+//
+// $("#contact ul:first-child").css({border: "2px solid red"});
+// $("#contact ul:last-child").css({border: "2px solid red"});
+//
+// // liczone od zera !!!
+// $("header nav li:even").css({border: "2px solid blue"});
+// $("header nav li:odd").css({border: "2px solid yellow"});
+
+// $("section:not('#contact')").css({border: "2px solid green"});
+
+// less then and greater then
+// od zera !!!
+// $("#social-nav li:lt(3)").css({border: "2px solid blue"});
+// $("#social-nav li:gt(2)").css({border: "2px solid blue"});
+
+// $("div[class]").css({border: "2px solid pink"});
+
+// $("img[alt=quote]").css({border: "2px solid purple"});
+
+*/
+
+
+/*
+//ep#5
+
 // znacznik
 $("h3").css({border: "3px solid blue"});
 
 // klasa
-$(".wrapper").css({border: "3px solid red"})
+$(".wrapper").css({border: "3px solid red"});
 
 // id
-$("#clients").css({border: "3px solid yellow"})
+$("#clients").css({border: "3px solid yellow"});
+*/
