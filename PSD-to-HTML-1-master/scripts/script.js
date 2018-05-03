b43ea1a450f46b4d2a6e7850a87cf37c77d34df7
Dorota Pawłowska <dpawlowska@op.pl>
2018-05-03 20:54:32 +0200

    ep#21 - Hide and Show Elements

--- PSD-to-HTML-1-master/scripts/script.js
+++ PSD-to-HTML-1-master/scripts/script.js
@@ -1,4 +1,22 @@
 
+//ep#21 - Hide and Show Elements
+/*
+$(document).ready(function () {
+    $("img[alt=map]").on("click", function () {
+        // $(this).hide(1000).show(1000);
+        $("section > h2").toggle(1000);
+    });
+});
+
+// .fadeIn();
+// .fadeOut();
+// .fadeTo();
+
+// .hide();
+// .show();
+// .toggle();
+*/
+
 //ep#20 - Fading Elements In & Out
 /*
 $(document).ready(function () {
