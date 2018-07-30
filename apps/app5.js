c0dbb1efc4a04e460bfb54a4616813eecc5ddf94
Dorota Pawłowska <dpawlowska@op.pl>
2018-07-30 21:57:10 +0200

    Node JS Tutorial for Beginners #15 - Writable Streams

--- apps/app5.js
+++ apps/app5.js
@@ -2,11 +2,13 @@ var http = require('http');
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream('../stuff/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream('../stuff/writeMe2.txt');
 var ch = 0;
 myReadStream.on('data', function (chunk) {
     ch+=1;
     console.log( ch + ' new chunk received: ');
-    console.log(chunk);
+    // console.log(chunk);
+    myWriteStream.write(chunk);
 });
 
 
