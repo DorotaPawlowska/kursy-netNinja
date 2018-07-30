77beedd8038c67062bed7df5378d6653423b2a9e
Dorota Pawłowska <dpawlowska@op.pl>
2018-07-30 13:26:58 +0200

    Node JS Tutorial for Beginners #7 - Module Patterns

--- app1.js
+++ app1.js
@@ -1,3 +1,6 @@
-var counter = require('./count');
+// var counter = require('./stuff');
+var stuff = require('./stuff');
 
-console.log(counter(['shaun', 'crystal', 'ryu']));
+console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
+console.log(stuff.adder(6,5));
+console.log(stuff.adder(stuff.pi, 5));
