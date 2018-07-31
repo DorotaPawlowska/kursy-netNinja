08817e2785f3a0992e9a2dd14366959b3209119e
Dorota Pawłowska <dpawlowska@op.pl>
2018-07-31 23:07:28 +0200

    Node JS Tutorial for Beginners #27 - Partial Templates

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     // res.send('this is the home page');
-    res.sendFile(__dirname + '/index.html');
+    // res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/contact', function (req, res) {
     // res.send('this is the contact page');
-    res.sendFile(__dirname + '/contact.html');
+    // res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
 });
 
 app.get('/profile/:name', function (req, res) {
