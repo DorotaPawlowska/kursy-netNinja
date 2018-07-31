4debf3d7a97aab661fa37ee9064c7079098d6a71
Dorota Pawłowska <dpawlowska@op.pl>
2018-07-31 19:43:08 +0200

    Node JS Tutorial for Beginners #25 - Template Engines

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ var  express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-    res.send('this is the home page');
+    // res.send('this is the home page');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function (req, res) {
-    res.send('this is the contact page');
+    // res.send('this is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', function (req, res) {
-    res.send('you req to see profile with the name of ' + req.params.name);
+    var data = {
+        age: 29,
+        job: 'ninja'
+    };
+
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
