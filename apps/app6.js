050af5c760f71027bf97eb8a34b485cb7c37f05d
Dorota Pawłowska <dpawlowska@op.pl>
2018-07-31 14:04:42 +0200

    Node JS Tutorial for Beginners #18 - Serving JSON Data

--- apps/app6.js
+++ apps/app6.js
@@ -1,19 +1,24 @@
 var http = require('http');
 var fs = require('fs');
 
-
-
 var server = http.createServer(function (request, response) {
     console.log('request was made: ' + request.url);
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, {'Content-Type': 'application/json'});
 
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(response);
+    var myObj = {
+        name: 'Ryu',
+        job: 'ninja',
+        age: 19
+    };
+
+    response.end(JSON.stringify(myObj));
+
+
+
+    // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    // myReadStream.pipe(response);
 });
 
 server.listen(3000, '127.0.0.1');
 
 console.log('yo dawgs, now listening to port 3000');
-
-
-
