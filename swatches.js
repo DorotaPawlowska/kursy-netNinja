0da530a175f1e2180beedd60b25e46948f5509a3
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-03 10:14:45 +0200

    CSS Variables Tutorial #4 - CSS Variables & JavaScript - without ES6

--- swatches.js
+++ swatches.js
@@ -1,8 +1,8 @@
 var swatches = document.querySelectorAll('.swatches span');
 var root = document.querySelector(':root');
 
-swatches.forEach((swatch) => {
-   swatch.addEventListener('click', (e) => {
+swatches.forEach(function(swatch){
+   swatch.addEventListener('click', function(e){
        root.style.setProperty('--theme-color', e.target.style.background)
     })
 });
