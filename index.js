efbe033685f7a5db939c94519f1aed7a3c4ea939
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-03 17:09:02 +0200

    REST API Tutorial (Node, Express & Mongo) #5 - Handling Requests

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const express = require('express');
 //set up express app
 const app = express();
 
+app.get('/api', function (req, res) {
+    console.log('GET req');
+    res.send({name: 'yoshi'});
+});
+
+
+
+
+
 //listen for requests
 app.listen( process.env.port || 4000, function () {
     console.log('hey we are ready for your requests on port 4000 or so')
 });
+
