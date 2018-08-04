cc8aabd219ae61d9d23685f3d4de7f853f3a8731
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-04 21:44:47 +0200

    REST API Tutorial (Node, Express & Mongo) #11 - Handling DELETE Requests

--- routes-trasy/api.js
+++ routes-trasy/api.js
@@ -27,7 +27,11 @@ router.put('/ninjas/:id', function (req, res, next) {
 
 //delete a ninja from the DB
 router.delete('/ninjas/:id', function (req, res, next) {
-    res.send({type: 'DELETE'});
+    // console.log(req.params.id);
+    Ninja.findByIdAndRemove({_id: req.params.id}).then(function (ninja) {
+        res.send(ninja);
+    });
+    // res.send({type: 'DELETE'});
 });
 
 module.exports = router;
