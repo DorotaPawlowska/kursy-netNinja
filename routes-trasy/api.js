2bf26b2a0936f203873f8b42db9372f66a1c44b5
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-04 21:53:00 +0200

    REST API Tutorial (Node, Express & Mongo) #12 - PUT Requests

--- routes-trasy/api.js
+++ routes-trasy/api.js
@@ -22,15 +22,19 @@ router.post('/ninjas', function (req, res, next) {
 
 //update a ninja in the DB
 router.put('/ninjas/:id', function (req, res, next) {
-    res.send({type: 'PUT'});
+    Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then(function (ninja) {
+        res.send(ninja);
+    })
 });
 
 //delete a ninja from the DB
 router.delete('/ninjas/:id', function (req, res, next) {
     // console.log(req.params.id);
-    Ninja.findByIdAndRemove({_id: req.params.id}).then(function (ninja) {
-        res.send(ninja);
-    });
+    Ninja.findByIdAndRemove({_id: req.params.id}).then(function () {
+        Ninja.findOne({_id: req.params.id}).then(function (ninja) {
+            res.send(ninja);
+        })
+    })
     // res.send({type: 'DELETE'});
 });
 
