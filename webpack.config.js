e659fe6e257f0babc52936ea941f738902ac8ee8
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-06 11:23:50 +0200

    Webpack Tutorial for Beginners #3 - The webpack.config File

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    //def entry point
+    entry:'./src/script-1.js',
+
+    //def output point
+    output: {
+        path: 'dist',
+        filename: 'bundle.js'
+    }
+};
