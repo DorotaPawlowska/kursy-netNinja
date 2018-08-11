798c5d528dc1f26a34c50be3db4568d0c9b35de8
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-11 23:36:46 +0200

    MongoDB Tutorial #7 - ES6 Promises

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,20 @@
 var mongoose = require('mongoose');
 
-//connect to MDB
-mongoose.connect('mongodb://localhost/testaroo');
+//es6 promises
+mongoose.Promise = global.Promise;
 
-mongoose.connection.once('open', function () {
-    console.log('connection has been made, whohoo!')
-}).on('error', function (error) {
-    console.log('connestion error: ', error);
+//connect to the database before tests run
+before(function (done) {
+
+    //connect to MDB
+    mongoose.connect('mongodb://localhost/testaroo');
+
+    mongoose.connection.once('open', function () {
+        console.log('connection has been made, whohoo!');
+        done();
+    }).on('error', function (error) {
+        console.log('connestion error: ', error);
+    });
+
 });
+
