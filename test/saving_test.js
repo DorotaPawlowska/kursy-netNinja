b8351bdefd2d74f85606dcbae91a23826f6dffab
Dorota Pawłowska <dpawlowska@op.pl>
2018-08-11 23:21:48 +0200

    MongoDB Tutorial #6 - Saving Data to MongoDB

---
+++ test/saving_test.js
@@ -0,0 +1,21 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+//describe tests
+describe('saving records', function () {
+
+    //create tests
+    it('saves record to the database', function (done) {
+
+        var char = new MarioChar({
+            name: 'Mario'
+        });
+        // asynchroniczne - zwraca promisę dlatego .then
+        char.save().then(function () {
+            assert(char.isNew === false);
+            done();
+        });
+    });
+    //next test...
+});
