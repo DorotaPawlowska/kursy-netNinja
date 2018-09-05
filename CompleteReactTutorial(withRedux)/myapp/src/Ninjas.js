2e1cbbab8cfa761ab85410935bc5e6b03878913b
Dorota Pawłowska <dpawlowska@op.pl>
2018-09-05 17:23:07 +0200

    Complete React Tutorial ( & Redux) #19 - Deleting Data
    Changes to be committed:
    	modified:   CompleteReactTutorial(withRedux)/myapp/src/App.js
    	modified:   CompleteReactTutorial(withRedux)/myapp/src/Ninjas.js

--- CompleteReactTutorial(withRedux)/myapp/src/Ninjas.js
+++ CompleteReactTutorial(withRedux)/myapp/src/Ninjas.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
-const Ninjas = ({ninjas}) => {
+const Ninjas = ({ninjas, deleteNinja}) => {
     // console.log(this.props);
 
     // const { ninjas } = props; // destrukturyzacja
-    // const ninjaList = ninjas.map( ninja => {
-    //     if(ninja.age > 20){
-    //         return (
-    //         <div className="ninja" key={ninja.id}>
-    //             <div> Name: { ninja.name } </div>
-    //             <div> Age: { ninja.age } </div>
-    //             <div> Belt: { ninja.belt } </div>
-    //         </div>
-    //         )
-    //     } else {
-    //         return null;
-    //     }
-    // })
+
+    const ninjaList = ninjas.map( ninja => {
+        if(ninja.age > 20){
+            return (
+            <div className="ninja" key={ninja.id}>
+                <div> Name: { ninja.name } </div>
+                <div> Age: { ninja.age } </div>
+                <div> Belt: { ninja.belt } </div>
+                <button onClick={ () => {deleteNinja(ninja.id)}}>Delete ninja</button>
+            </div>
+            )
+        } else {
+            return null;
+        }
+    })
+
+    return(
+        <div className="ninja-list">
+            { ninjaList }
+        </div>
+    )
 
     // const ninjaList = ninjas.map(ninja => {
     //     return ninja.age > 20 ? (
@@ -28,22 +36,22 @@ const Ninjas = ({ninjas}) => {
     //     ) : null;
     // })
 
-  return (
-      <div className="ninja-list">
-      {/* { ninjaList} albo to co jest na dole */}
-      {
-          ninjas.map(ninja => {
-              return ninja.age > 20 ? (
-                  <div className="ninja" key={ninja.id}>
-                      <div> Name: { ninja.name } </div>
-                      <div> Age: { ninja.age } </div>
-                      <div> Belt: { ninja.belt } </div>
-                  </div>
-              ) : null;
-          })
-      }
-      </div>
-  )
+//   return (
+//       <div className="ninja-list">
+//       {/* { ninjaList} albo to co jest na dole */}
+//       {
+//           ninjas.map(ninja => {
+//               return ninja.age > 20 ? (
+//                   <div className="ninja" key={ninja.id}>
+//                       <div> Name: { ninja.name } </div>
+//                       <div> Age: { ninja.age } </div>
+//                       <div> Belt: { ninja.belt } </div>
+//                   </div>
+//               ) : null;
+//           })
+//       }
+//       </div>
+//   )
 }
 
 export default Ninjas;
