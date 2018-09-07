e25b8485d6db1f9d9abe613af19b9fb2c631b155
Dorota Pawłowska <dpawlowska@op.pl>
2018-09-07 20:38:38 +0200

    Complete React Tutorial (& Redux) #25 - The React Router
    Changes to be committed:
    	new file:   CompleteReactTutorial(withRedux)/poketimes/.gitignore
    	new file:   CompleteReactTutorial(withRedux)/poketimes/README.md
    	new file:   CompleteReactTutorial(withRedux)/poketimes/package.json
    	new file:   CompleteReactTutorial(withRedux)/poketimes/public/favicon.ico
    	new file:   CompleteReactTutorial(withRedux)/poketimes/public/index.html
    	new file:   CompleteReactTutorial(withRedux)/poketimes/public/manifest.json
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/App.js
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/components/About.js
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/components/Contact.js
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/components/Home.js
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/components/Navbar.js
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/index.css
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/index.js
    	new file:   CompleteReactTutorial(withRedux)/poketimes/src/registerServiceWorker.js

---
+++ CompleteReactTutorial(withRedux)/poketimes/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return(
+    <div className="container">
+      <h4 className="center">
+        About
+      </h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum illum molestias porro aperiam voluptas quibusdam fugit repellendus itaque rerum voluptate eligendi dolor, non optio at odit a? Beatae, veritatis illum.</p>
+    </div>
+  )
+}
+
+export default About;
---
+++ CompleteReactTutorial(withRedux)/poketimes/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return(
+    <div className="container">
+      <h4 className="center">
+        Home
+      </h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum illum molestias porro aperiam voluptas quibusdam fugit repellendus itaque rerum voluptate eligendi dolor, non optio at odit a? Beatae, veritatis illum.</p>
+    </div>
+  )
+}
+
+export default Home;
