b80e8392c7f769fa1a93dfa719d908d7c5142360
Dorota Pawłowska <dpawlowska@op.pl>
2018-10-24 21:26:50 +0200

    React, Redux & Firebase App Tutorial #12 - Adding Dummy Data
    Changes to be committed:
    	modified:   src/components/dashboard/Dashboard.js
    	modified:   src/components/projects/ProjectList.js
    	modified:   src/components/projects/ProjectSummary.js
    	modified:   src/store/reducers/projectReducer.js

--- ReactRedux&FirebaseAppTutorial/marioplan/src/components/projects/ProjectSummary.js
+++ ReactRedux&FirebaseAppTutorial/marioplan/src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (
       <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
-          <span className="cad-title">Project Title</span>
+          <span className="cad-title">{project.title}</span>
           <p>Posted by the nn </p>
           <p className="grey-text">3rd Sep, 2am</p>
         </div>
--- ReactRedux&FirebaseAppTutorial/marioplan/src/store/reducers/projectReducer.js
+++ ReactRedux&FirebaseAppTutorial/marioplan/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: '1', title: 'help me', content: 'blah '},
+    { id: '2', title: 'help me two', content: 'blah blah'},
+    { id: '3', title: 'help me tree', content: 'blah blah blah'}
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
