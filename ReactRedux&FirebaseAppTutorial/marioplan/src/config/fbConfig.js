4244e458641807adf920bddc9dd0ec70195763e2
Dorota Pawłowska <dpawlowska@op.pl>
2018-10-25 21:07:10 +0200

    React, Redux & Firebase App Tutorial #15 - Creating a Firebase Project
    Changes to be committed:
    	modified:   package.json
    	new file:   src/config/fbConfig.js

---
+++ ReactRedux&FirebaseAppTutorial/marioplan/src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebas/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA1WqRhtvtHti4obkdn0JggE1zxuKmShVk",
+  authDomain: "nn-marioplan.firebaseapp.com",
+  databaseURL: "https://nn-marioplan.firebaseio.com",
+  projectId: "nn-marioplan",
+  storageBucket: "nn-marioplan.appspot.com",
+  messagingSenderId: "80142120845"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true});
+
+export default firebase;
