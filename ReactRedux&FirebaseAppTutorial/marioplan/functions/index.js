2b6eafa3042636e49421916d373d382a3bba3f00
Dorota Pawłowska <dpawlowska@op.pl>
2018-11-04 15:33:36 +0100

    React, Redux & Firebase App Tutorial #37 - Cloud Function Auth Trigger
    Changes to be committed:
    	modified:   functions/index.js

--- ReactRedux&FirebaseAppTutorial/marioplan/functions/index.js
+++ ReactRedux&FirebaseAppTutorial/marioplan/functions/index.js
@@ -6,11 +6,11 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello, ninjas");
 });
 
-const createNotification = (notification) => {
+const createNotification = (notification => {
   return admin.firestore().collection('notifications')
     .add(notification)
     .then(doc => console.log('notification added', doc));
-}
+})
 
 exports.projectCreated = functions.firestore
   .document('projects/{projectId}')
@@ -23,5 +23,19 @@ exports.projectCreated = functions.firestore
       time: admin.firestore.FieldValue.serverTimestamp()
     }
     return createNotification(notification);
+});
 
+exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    return admin.firestore().collection('users')
+    .doc(user.uid).get().then(doc => {
+
+      const newUser = doc.data();
+      const notification = {
+        content: 'Joined the party',
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      }
+      return createNotification(notification);
+    })
 })
