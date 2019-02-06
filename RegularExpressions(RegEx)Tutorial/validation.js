0ffd0471580ba8e4ad1257955aba29e285ba3e1e
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-06 19:34:50 +0100

    Regular Expressions (RegEx) Tutorial #13 - Testing a RegEx Pattern
    Changes to be committed:
    	modified:   RegularExpressions(RegEx)Tutorial/validation.js

--- RegularExpressions(RegEx)Tutorial/validation.js
+++ RegularExpressions(RegEx)Tutorial/validation.js
@@ -1,8 +1,26 @@
 // https://regex101.com/
 
-var reg = /[a-z]/gi;
-var reg2 = new RegExp(/[a-z]/, 'gi');
+// var reg = /[a-z]/gi;
+// var reg2 = new RegExp(/[a-z]/, 'gi');
+
+const inputs = document.querySelectorAll('input');
 
 const patterns = {
     telephone: /^\d{11}$/
 };
+
+//validation function
+function validate(field, regex){
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    }else{
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        // console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    });
+});
