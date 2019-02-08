a8d453e207659a0822e0e9c853c0ef4a6b35a95b
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-08 20:09:23 +0100

    Firebase Auth Tutorial #6 - Signing Users Out
    Changes to be committed:
    	modified:   FirebaseAuthTutorial/scripts/auth.js

--- FirebaseAuthTutorial/scripts/auth.js
+++ FirebaseAuthTutorial/scripts/auth.js
@@ -19,4 +19,14 @@ signupForm.addEventListener('submit', (e) => {
             signupForm.reset();
         });
 
-})
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut()
+        .then(() => {
+            console.log('user logout');
+        });
+});
