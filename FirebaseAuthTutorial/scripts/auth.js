12e053283773ba0e2d7880887a97902426df1579
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-10 13:22:02 +0100

    Firebase Auth Tutorial #12 - Adding Guides
    Changes to be committed:
    	modified:   FirebaseAuthTutorial/scripts/auth.js

--- FirebaseAuthTutorial/scripts/auth.js
+++ FirebaseAuthTutorial/scripts/auth.js
@@ -18,6 +18,25 @@ auth.onAuthStateChanged(user => {
 
 })
 
+//create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    db.collection('guides').add({
+        title: createForm['title'].value, // można tak []
+        content: createForm.content.value // albo tak . 
+    }).then(() => {
+        //close the modal and reset form
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log(err.message);
+    })
+})
+
+
 
 // sign up
 
