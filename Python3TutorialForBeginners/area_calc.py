a6b2b8a19fe2ac8e1f4b50a3841af20446afe464
dorota <dpawlowska@op.pl>
2019-03-17 13:19:54 +0100

    Python 3 Tutorial for Beginners #6 - Standard Input
    Changes to be committed:
    	new file:   area_calc.py

---
+++ Python3TutorialForBeginners/area_calc.py
@@ -0,0 +1,9 @@
+# comments
+# name = input('Tell me your name, punk: ')
+# age = input('..and your age: ')
+# print(name, 'you are', age)
+
+# calc the area of the circle
+radius = input('enter the radius of your circle(m): ')
+area = 3.142 * int(radius)**2
+print('the area of your circle id:', area)
