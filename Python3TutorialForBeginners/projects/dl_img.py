a703b80927500085127ba70e659dc4a918612bcc
dorota <dpawlowska@op.pl>
2019-03-23 17:30:42 +0100

    Python 3 Tutorial for Beginners #29 - Downloading Images
    Changes to be committed:
    	new file:   Python3TutorialForBeginners/projects/dl_img.py
    	new file:   Python3TutorialForBeginners/projects/images/codeChallende.jpg

---
+++ Python3TutorialForBeginners/projects/dl_img.py
@@ -0,0 +1,10 @@
+import urllib.request
+
+def dl_jpg(url, file_path, file_name):
+    full_path = file_path + file_name + '.jpg'
+    urllib.request.urlretrieve(url, full_path)
+
+url = input('enter img URL to download: ')
+file_name = input('enter file name to save as: ')
+
+dl_jpg(url, 'images/', file_name)
