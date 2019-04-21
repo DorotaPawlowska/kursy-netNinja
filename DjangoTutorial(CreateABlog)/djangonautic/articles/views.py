babe264a9bbf8979ed0b65eeba9b39e7e7390837
dorota <dpawlowska@op.pl>
2019-04-21 14:16:32 +0200

    Django Tutorial #16 - Article Detail Template
    Changes to be committed:
    	new file:   DjangoTutorial(CreateABlog)/djangonautic/articles/templates/articles/article_detail.html
    	modified:   DjangoTutorial(CreateABlog)/djangonautic/articles/views.py

--- DjangoTutorial(CreateABlog)/djangonautic/articles/views.py
+++ DjangoTutorial(CreateABlog)/djangonautic/articles/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 def article_list(request):
     articles = Article.objects.all().order_by('date')
-    return render(request, 'articles/article_list.html', {'articles': articles})
+    return render(request, 'articles/article_list.html', { 'articles': articles} )
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', { 'article': article })
