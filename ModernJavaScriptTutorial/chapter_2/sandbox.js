8d4d7591e4d732d68ea449e24212f8f2d25ecb0e
dorota <dpawlowska@op.pl>
2019-05-25 22:01:11 +0200

    Modern JavaScript Tutorial #2 - Syntax Basics & Types - chapter_2
    Changes to be committed:
    	new file:   ModernJavaScriptTutorial/chapter_2/index.html
    	new file:   ModernJavaScriptTutorial/chapter_2/sandbox.js

---
+++ ModernJavaScriptTutorial/chapter_2/sandbox.js
@@ -0,0 +1,24 @@
+// alert('hello!');
+
+
+// log things to console
+
+/*  console.log(1);
+    console.log(2);
+*/
+
+let age = 25;
+let year = 2019;
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+// points = 50;
+// console.log(points);
+
+var score = 75;
+console.log(score);
