05ba4d766ef541a04c329af712742f3f00c5eacd
dorota <dpawlowska@op.pl>
2019-05-28 21:11:52 +0200

    Modern JavaScript Tutorial #2 - Syntax Basics & Types - strings
    Changes to be committed:
    	modified:   ModernJavaScriptTutorial/chapter_2/index.html
    	new file:   ModernJavaScriptTutorial/chapter_2/sandbox2.js

---
+++ ModernJavaScriptTutorial/chapter_2/sandbox2.js
@@ -0,0 +1,37 @@
+// strings
+console.log('hello');
+
+let email = 'email@email.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
+
+// common string methods
+let mail = 'mario@test.co.uk';
+
+// let res = mail.lastIndexOf('o');
+// let res = mail.slice(0,5);
+// let res = mail.substr(6, 4);
+// let res = mail.replace('m', 'w');
+// let res = mail.replace('o', 'w');
+
+console.log(res);
