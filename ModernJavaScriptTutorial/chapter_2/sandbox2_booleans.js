1a143bd08e295fe0d46e2caa723ade39ee4a181b
dorota <dpawlowska@op.pl>
2019-06-02 16:21:10 +0200

    Modern JavaScript Tutorial #2 - Syntax Basics & Types - comparision & type conversion
    Changes to be committed:
    	modified:   ModernJavaScriptTutorial/chapter_2/sandbox2_booleans.js

--- ModernJavaScriptTutorial/chapter_2/sandbox2_booleans.js
+++ ModernJavaScriptTutorial/chapter_2/sandbox2_booleans.js
@@ -1,5 +1,5 @@
 // ------- booleans & comparisions
-console.log(true, false, "true", "false");
+// console.log(true, false, "true", "false");
 
 // ------- methods can return booleans
 // let email = 'luigi@test.co.uk';
@@ -22,10 +22,45 @@ console.log(true, false, "true", "false");
 // console.log(age <= 25);
 // console.log(age >= 25);
 
-let name = 'shaun';
+// let name = 'shaun';
 
-console.log(name == 'shaun');
-console.log(name == 'Shaun');
-console.log(name > 'crystal');
-console.log(name > 'Shaun');
-console.log(name > 'Crystal');
+// console.log(name == 'shaun');
+// console.log(name == 'Shaun');
+// console.log(name > 'crystal');
+// console.log(name > 'Shaun');
+// console.log(name > 'Crystal');
+
+// =================== COMPARISON =========================
+
+let age = 25;
+
+// loose
+// console.log(age == 25);
+// console.log(age == '25');
+// console.log(age != 25);
+// console.log(age != '25');
+
+// strict
+console.log(age === 25);
+console.log(age === '25');
+console.log(age !== 25);
+console.log(age !== '25');
+
+// =================== TYPE CONVERSION =========================
+let score = '100';
+
+score = Number(score);
+
+console.log(score + 1);
+console.log(typeof score);
+
+// let result = 'hello';
+// result = Number(result);
+
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean(0);
+// let result = Boolean('0');
+let result = Boolean('');
+
+console.log(result, typeof result);
