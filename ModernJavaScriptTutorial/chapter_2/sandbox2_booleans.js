09348f9f63832a02c5dbb7a44ac4406cea82a492
dorota <dpawlowska@op.pl>
2019-06-02 16:06:33 +0200

    Modern JavaScript Tutorial #2 - Syntax Basics & Types - booleans
    Changes to be committed:
    	modified:   ModernJavaScriptTutorial/chapter_2/index.html
    	new file:   ModernJavaScriptTutorial/chapter_2/sandbox2_booleans.js

---
+++ ModernJavaScriptTutorial/chapter_2/sandbox2_booleans.js
@@ -0,0 +1,31 @@
+// ------- booleans & comparisions
+console.log(true, false, "true", "false");
+
+// ------- methods can return booleans
+// let email = 'luigi@test.co.uk';
+// let result = email.includes('#');
+
+// let names = ['mario', 'luigi', 'toad'];
+// let result = names.includes('bowser');
+
+// console.log(result);
+
+// ------- comparision operators
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age != 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 25);
+
+let name = 'shaun';
+
+console.log(name == 'shaun');
+console.log(name == 'Shaun');
+console.log(name > 'crystal');
+console.log(name > 'Shaun');
+console.log(name > 'Crystal');
