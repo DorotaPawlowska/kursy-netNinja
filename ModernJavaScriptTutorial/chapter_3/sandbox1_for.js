ea784681f4ee62a891c4649e26a59ee73ab4574a
dorota <dpawlowska@op.pl>
2019-06-13 20:58:27 +0200

    Modern JavaScript Tutorial #3 - Control Flow - for loop
    Changes to be committed:
    	new file:   ModernJavaScriptTutorial/chapter_3/index.html
    	new file:   ModernJavaScriptTutorial/chapter_3/sandbox1_for.js

---
+++ ModernJavaScriptTutorial/chapter_3/sandbox1_for.js
@@ -0,0 +1,17 @@
+// for loops
+
+
+// for(let i = 0; i < 5; i++){
+//     console.log('in loop: ' ,i);
+// }
+
+// console.log('loop finished');
+
+const names = ['shaun','mario','luigi'];
+
+for(let j = 0; j < names.length; j++){
+    // console.log(names[j]);
+    // console.log(j);
+    let html = `<div>${names[j]}</div>`;
+    console.log(html);
+}
