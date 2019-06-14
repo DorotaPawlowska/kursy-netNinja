a5e851d998280c071a48533bdd7302d9d1726420
dorota <dpawlowska@op.pl>
2019-06-14 20:59:43 +0200

    Modern JavaScript Tutorial #3 - Control Flow - while loop
    Changes to be committed:
    	modified:   ModernJavaScriptTutorial/chapter_3/index.html
    	modified:   ModernJavaScriptTutorial/chapter_3/sandbox1_for.js
    	new file:   ModernJavaScriptTutorial/chapter_3/sandbox2_while.js

---
+++ ModernJavaScriptTutorial/chapter_3/sandbox2_while.js
@@ -0,0 +1,22 @@
+// while loop
+
+// for(let i = 0; i < 5; i++){
+//     console.log('in loop: ', i);
+// }
+
+const names = ['shaun', 'mario', 'luigi'];
+
+let j = 0;
+
+while(j < names.length){
+    console.log(names[j]);
+    j++;
+}
+
+
+
+// let i = 0;
+// while(i < 5){
+//     console.log('in while loop: ', i);
+//     i++;
+// }
