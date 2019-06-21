d72e89f48f10e9ecf42526612b852d33748949f1
dorota <dpawlowska@op.pl>
2019-06-21 13:49:30 +0200

    Modern JavaScript Tutorial #4 - Functions - callbackFunctions
    Changes to be committed:
    	modified:   ModernJavaScriptTutorial/chapter_4/index.html
    	new file:   ModernJavaScriptTutorial/chapter_4/sandbox6_callbacks.js

---
+++ ModernJavaScriptTutorial/chapter_4/sandbox6_callbacks.js
@@ -0,0 +1,21 @@
+// callbacks & foreach
+
+const myFunc = (callbackFunc) => {
+    // do something
+    let value = 50;
+    callbackFunc(value);
+};
+
+myFunc((value) => console.log(value));
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+    console.log(`${index} - hello ${person}`);
+};
+
+people.forEach(logPerson);
+// people.forEach((person, index) => console.log(index, person));
+
+
+
