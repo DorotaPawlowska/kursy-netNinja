9742c9afcb86d054b1f43625bf00d356fa79e4d4
dorota <dpawlowska@op.pl>
2019-06-21 15:16:38 +0200

    Modern JavaScript Tutorial #5 - Objects - methods
    Changes to be committed:
    	modified:   ModernJavaScriptTutorial/chapter_5/sandbox1_objLiterals.js

--- ModernJavaScriptTutorial/chapter_5/sandbox1_objLiterals.js
+++ ModernJavaScriptTutorial/chapter_5/sandbox1_objLiterals.js
@@ -1,12 +1,40 @@
-// object literals
+// === object literals
 
 let user = {
     name: 'crystal',
     age: 30,
     email: 'crystal@thenetninja.co.uk',
     loaction: 'Berlin',
-    blogs: ['why mac & chees rules', '10 things to make with marmite']
+    blogs: ['why mac & chees rules', '10 things to make with marmite'],
+    login: function(){
+        console.log('the user logged in');
+    },
+    logout: function(){
+        console.log('the user logged out');
+    },
+    logBlogs: function(){
+        
+    }
 };
+
+// === object methods
+
+user.login();
+user.logout();
+
+
+// const name = 'mario';
+// name.toUpperCase();
+
+
+
+
+
+
+
+
+
+// === object properties
 
 console.log(user);
 console.log(user.name);
