0a8ee368e2cf31f6a6cb3bca4900c57d0bae9d5b
dorota <dpawlowska@op.pl>
2019-06-22 21:31:52 +0200

    Modern JavaScript Tutorial #6 - The Document Object Model - querySelectors
    Changes to be committed:
    	new file:   ModernJavaScriptTutorial/chapter_6/index.html
    	new file:   ModernJavaScriptTutorial/chapter_6/sandbox.js

---
+++ ModernJavaScriptTutorial/chapter_6/sandbox.js
@@ -0,0 +1,19 @@
+const para = document.querySelector('p');
+const pErr = document.querySelector('.error');
+const divErr = document.querySelector('div.error');
+const hBody = document.querySelector('body > h1');
+
+// console.log(para);
+
+const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+
+console.log(errors);
+
+paras.forEach(para => {
+    // console.log(para);
+});
+
+// console.log(paras);
+// console.log(paras[0]);
+// console.log(paras[2]);
