bbff6cf971ba81e9d019872d8c04fc78b95b67d8
dorota <dpawlowska@op.pl>
2019-06-22 21:44:31 +0200

    Modern JavaScript Tutorial #6 - The Document Object Model - get element by
    Changes to be committed:
    	modified:   ModernJavaScriptTutorial/chapter_6/index.html
    	renamed:    ModernJavaScriptTutorial/chapter_6/sandbox.js -> ModernJavaScriptTutorial/chapter_6/sandbox1_querySel.js
    	new file:   ModernJavaScriptTutorial/chapter_6/sandbox2_elementBy.js

---
+++ ModernJavaScriptTutorial/chapter_6/sandbox2_elementBy.js
@@ -0,0 +1,20 @@
+// ----- get an element by ID
+const title = document.getElementById('page-title');
+console.log(title);
+
+
+// ----- get element by their class name
+const errors = document.getElementsByClassName('error');
+console.log(errors);
+// console.log(errors[0]);
+
+// errors.forEach(element => {
+//     console.log(element);
+// });
+
+
+// ----- get elements by their tag name
+const paras = document.getElementsByTagName('p');
+
+console.log(paras);
+console.log(paras[1]);
