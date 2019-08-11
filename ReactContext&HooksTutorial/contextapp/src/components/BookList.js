ebe4d1d39fb01bdd28ad3de119bae7aebf4ff5bf
dorota <dpawlowska@op.pl>
2019-08-11 15:03:01 +0200

    React Context & Hooks Tutorial #15 - Creating Context with Functional Components
    Changes to be committed:
    	modified:   ReactContext&HooksTutorial/contextapp/src/App.js
    	modified:   ReactContext&HooksTutorial/contextapp/src/components/BookList.js
    	new file:   ReactContext&HooksTutorial/contextapp/src/contexts/BookContext.js

--- ReactContext&HooksTutorial/contextapp/src/components/BookList.js
+++ ReactContext&HooksTutorial/contextapp/src/components/BookList.js
@@ -1,6 +1,7 @@
 // import React, { Component, useContext } from 'react';
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 // class BookList extends Component {
 
@@ -24,14 +25,17 @@ import { ThemeContext } from '../contexts/ThemeContext';
 const BookList = () => {
 
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
 
   return (
     <div style={{ color: theme.syntax, background: theme.bg }} className="book-list">
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {books.map( book => {
+          return (
+            <li style={{ background: theme.ui }} key={book.id}>{ book.title }</li>
+          )
+        })}
       </ul>
     </div>
   );
