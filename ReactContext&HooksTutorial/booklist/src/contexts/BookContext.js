a73ff36110e603c42d366b6c855a8ed9b87d363f
dorota <dpawlowska@op.pl>
2019-08-15 16:06:45 +0200

    React Context & Hooks Tutorial #20 - Adding a Reducer
    Changes to be committed:
    	modified:   booklist/src/components/BookDetails.js
    	modified:   booklist/src/components/BookForm.js
      modified:   booklist/src/contexts/BookContext.js
    	new file:   booklist/src/reducers/bookReducer.js

--- ReactContext&HooksTutorial/booklist/src/contexts/BookContext.js
+++ ReactContext&HooksTutorial/booklist/src/contexts/BookContext.js
@@ -1,24 +1,27 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid/v1';
+// import React, { createContext, useState, useReducer } from 'react';
+import React, { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
+// import uuid from 'uuid/v1';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [ books, setBooks ] = useState([
-    { title: 'book one', author: 'author one', id: 1},
-    { title: 'book two', author: 'author two', id: 2}
+  // const [ books, setBooks ] = useState([
+  const [ books, dispatch ] = useReducer(bookReducer, [
+    // { title: 'book one', author: 'author one', id: 1},
+    // { title: 'book two', author: 'author two', id: 2}
   ]);
 
-  const addBook = (title, author) => {
-    setBooks([...books, {title, author, id: uuid()}])
-  };
+  // const addBook = (title, author) => {
+  //   setBooks([...books, {title, author, id: uuid()}])
+  // };
 
-  const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id));
-  };
+  // const removeBook = (id) => {
+  //   setBooks(books.filter(book => book.id !== id));
+  // };
 
   return(
-    <BookContext.Provider value={{books, addBook, removeBook}}> 
+    <BookContext.Provider value={{books, dispatch}}> 
       { props.children }
     </BookContext.Provider>
   );
