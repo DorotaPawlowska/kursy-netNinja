cd2c7e4c6e491e592c98360ada120bae4bfee092
dorota <dpawlowska@op.pl>
2019-08-15 16:27:37 +0200

    React Context & Hooks Tutorial #21 - Adding Local Storage
    Changes to be committed:
    	modified:   booklist/src/contexts/BookContext.js

--- ReactContext&HooksTutorial/booklist/src/contexts/BookContext.js
+++ ReactContext&HooksTutorial/booklist/src/contexts/BookContext.js
@@ -1,24 +1,17 @@
-// import React, { createContext, useState, useReducer } from 'react';
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
-// import uuid from 'uuid/v1';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  // const [ books, setBooks ] = useState([
-  const [ books, dispatch ] = useReducer(bookReducer, [
-    // { title: 'book one', author: 'author one', id: 1},
-    // { title: 'book two', author: 'author two', id: 2}
-  ]);
+  const [ books, dispatch ] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
 
-  // const addBook = (title, author) => {
-  //   setBooks([...books, {title, author, id: uuid()}])
-  // };
-
-  // const removeBook = (id) => {
-  //   setBooks(books.filter(book => book.id !== id));
-  // };
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books))
+  }, [books]);
 
   return(
     <BookContext.Provider value={{books, dispatch}}> 
