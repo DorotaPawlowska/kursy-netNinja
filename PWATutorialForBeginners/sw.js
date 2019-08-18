8a1fac570104909e9187a1d0e0d6e45ed4b41818
dorota <dpawlowska@op.pl>
2019-08-18 14:35:08 +0200

    PWA Tutorial for Beginners #15 - Pre-Caching Assets
    Changes to be committed:
    	modified:   PWATutorialForBeginners/index.html
    	modified:   PWATutorialForBeginners/sw.js

--- PWATutorialForBeginners/sw.js
+++ PWATutorialForBeginners/sw.js
@@ -1,14 +1,37 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/PWATutorialForBeginners/',
+  '/PWATutorialForBeginners/index.html',
+  '/PWATutorialForBeginners/js/app.js',
+  '/PWATutorialForBeginners/js/ui.js',
+  '/PWATutorialForBeginners/js/materialize.min.js',
+  '/PWATutorialForBeginners/css/styles.css',
+  '/PWATutorialForBeginners/css/materialize.min.css',
+  '/PWATutorialForBeginners/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-  console.log('service worker has been installed')
+  // console.log('service worker has been installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then( cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  )
+
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker has been activated')
+  // console.log('service worker has been activated');
+
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
+
 });
+
