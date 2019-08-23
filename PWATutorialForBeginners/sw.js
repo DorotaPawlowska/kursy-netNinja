bfe89d0efee4814588f7803883f16a25d6224960
dorota <dpawlowska@op.pl>
2019-08-23 20:16:23 +0200

    PWA Tutorial for Beginners #21 - Limiting Cache Size
    Changes to be committed:
    	modified:   PWATutorialForBeginners/sw.js

--- PWATutorialForBeginners/sw.js
+++ PWATutorialForBeginners/sw.js
@@ -14,6 +14,17 @@ const assets = [
   '/PWATutorialForBeginners/pages/fallback.html'
 ];
 
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then( keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    })
+  })
+};
+
 // install service worker
 self.addEventListener('install', evt => {
   // console.log('service worker has been installed');
@@ -46,6 +57,7 @@ self.addEventListener('fetch', evt => {
       return cacheRes || fetch(evt.request).then( fetchRes => {
         return caches.open(dynamicCacheName).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
+          limitCacheSize(dynamicCacheName, 3);
           return fetchRes;
         })
       });
