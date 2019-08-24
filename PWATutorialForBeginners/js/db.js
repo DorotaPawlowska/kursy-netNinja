7efb84a7472ad66a4d1712679d51d243886ac785
dorota <dpawlowska@op.pl>
2019-08-24 15:17:45 +0200

    PWA Tutorial for Beginners #24 - Realtime Data
    Changes to be committed:
    	modified:   PWATutorialForBeginners/js/db.js
    	modified:   PWATutorialForBeginners/sw.js

---
+++ PWATutorialForBeginners/js/db.js
@@ -0,0 +1,13 @@
+// real-time litner
+db.collection('recipes').onSnapshot((snapshot) => {
+  // console.log(snapshot.docChanges());  
+  snapshot.docChanges().forEach(change => {
+    // console.log(change, change.doc.data(), change.doc.id);
+    if(change.type === 'added'){
+      // add doc data to web page 
+    }
+    if(change.type === 'removed'){
+      // remove the doc data from web page
+    }
+  });
+})
