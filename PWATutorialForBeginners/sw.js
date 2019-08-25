62a9cd707e6b325b3f986a7fc52ab49a8049249b
dorota <dpawlowska@op.pl>
2019-08-25 18:13:47 +0200

    PWA Tutorial for Beginners #26 - Offline Data
    Changes to be committed:
    	modified:   PWATutorialForBeginners/js/db.js
    	modified:   PWATutorialForBeginners/sw.js

--- PWATutorialForBeginners/sw.js
+++ PWATutorialForBeginners/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v2';
-const dynamicCacheName = 'site-dynamic-v1';
+const staticCacheName = 'site-static-v3';
+const dynamicCacheName = 'site-dynamic-v2';
 const assets = [
   '/PWATutorialForBeginners/',
   '/PWATutorialForBeginners/index.html',
@@ -52,20 +52,22 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
   // console.log('fetch event', evt);
-  // evt.respondWith(
-  //   caches.match(evt.request).then( cacheRes => {
-  //     return cacheRes || fetch(evt.request).then( fetchRes => {
-  //       return caches.open(dynamicCacheName).then(cache => {
-  //         cache.put(evt.request.url, fetchRes.clone());
-  //         limitCacheSize(dynamicCacheName, 15);
-  //         return fetchRes;
-  //       })
-  //     });
-  //   }).catch(() => {
-  //     if(evt.request.url.indexOf('.html') > -1){
-  //       return caches.match('/PWATutorialForBeginners/pages/fallback.html');
-  //     }
-  //   })
-  // );
+  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then( cacheRes => {
+        return cacheRes || fetch(evt.request).then( fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            limitCacheSize(dynamicCacheName, 15);
+            return fetchRes;
+          })
+        });
+      }).catch(() => {
+        if(evt.request.url.indexOf('.html') > -1){
+          return caches.match('/PWATutorialForBeginners/pages/fallback.html');
+        }
+      })
+    );
+  }  
 });
 
