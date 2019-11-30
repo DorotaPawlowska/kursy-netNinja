64d607fd75d8da4c9f26e29950a3b3541b43a7ab
dorota <dpawlowska@op.pl>
2019-11-30 20:58:55 +0100

    Flutter Tutorial for Beginners #8 - Images & Assets
    Changes to be committed:
    	new file:   FlutterTutorialForBeginners/myapp/assets/photo1.jpg
    	new file:   FlutterTutorialForBeginners/myapp/assets/photo2.jpg
    	new file:   FlutterTutorialForBeginners/myapp/assets/photo3.jpg
    	modified:   FlutterTutorialForBeginners/myapp/lib/main.dart
    	modified:   FlutterTutorialForBeginners/myapp/pubspec.yaml

--- FlutterTutorialForBeginners/myapp/lib/main.dart
+++ FlutterTutorialForBeginners/myapp/lib/main.dart
@@ -14,20 +14,11 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          'hello ninjas!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
-        ),
+        child: Image.asset('assets/photo1.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
-        child: Text('click me'),
+        child: Text('click'),
         backgroundColor: Colors.red[600],
       ),
     );
