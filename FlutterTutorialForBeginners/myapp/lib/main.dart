6710362415f22c45ff59d044827f37895a7c3693
dorota <dpawlowska@op.pl>
2019-11-30 21:56:49 +0100

    Flutter Tutorial for Beginners #9 - Buttons & Icons
    Changes to be committed:
    	modified:   FlutterTutorialForBeginners/myapp/lib/main.dart

--- FlutterTutorialForBeginners/myapp/lib/main.dart
+++ FlutterTutorialForBeginners/myapp/lib/main.dart
@@ -14,7 +14,45 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.asset('assets/photo1.jpg'),
+        child: IconButton(
+          onPressed: (){
+            print('you mailed me');
+          },
+          icon: Icon(Icons.alternate_email),
+          color: Colors.amber,
+        ),
+
+//        child: RaisedButton.icon(
+//          onPressed: (){},
+//          icon: Icon(
+//            Icons.mail
+//          ),
+//          label: Text('mail me'),
+//          color: Colors.amber,
+//        ),
+
+//        child: FlatButton(
+//          onPressed: (){
+//            print('you clicked me');
+//          },
+//          child: Text('click me'),
+//          color: Colors.lightBlue,
+//        ),
+
+//        child: RaisedButton(
+//          onPressed: (){},
+//          child: Text('click me'),
+//          color: Colors.lightBlue,
+//        ),
+
+//        child: Icon(
+//          Icons.airport_shuttle,
+//          color: Colors.lightBlue,
+//          size: 50,
+//        ),
+
+//        child: Image.asset('assets/photo1.jpg'),
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
