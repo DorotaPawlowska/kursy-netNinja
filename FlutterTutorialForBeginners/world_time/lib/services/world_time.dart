9d98e924f294ea9ba8cee2607b49de25b70969d2
dorota <dpawlowska@op.pl>
2019-12-25 23:43:50 +0100

    Flutter Tutorial for Beginners #29 - Error Handling
    Changes to be committed:
    	modified:   FlutterTutorialForBeginners/world_time/lib/services/world_time.dart

--- FlutterTutorialForBeginners/world_time/lib/services/world_time.dart
+++ FlutterTutorialForBeginners/world_time/lib/services/world_time.dart
@@ -10,25 +10,29 @@ class WorldTime{
   WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
-
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
+    
+    try{
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
 //    print(data);
-    // get properties from data
+      // get properties from data
 
-    String datetime= data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+      String datetime= data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
 
 //    print(datetime);
 //    print(offset);
 
-    // create datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // create datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
 
-    // set the time property
-    time = now.toString();
-
+      // set the time property
+      time = now.toString();
+    }catch(e){
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
   }
 }
 
