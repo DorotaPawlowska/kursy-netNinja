77f7f54e5ff51ccba0e61e20593f9aa10396e4b0
dorota <dpawlowska@op.pl>
2019-12-27 00:32:43 +0100

    Flutter & Firebase App Tutorial #4 - Firebase Auth
    Changes to be committed:
    	modified:   Flutter_FirebaseAppTutorial/brew_crew/lib/screens/wrapper.dart
    	new file:   Flutter_FirebaseAppTutorial/brew_crew/lib/services/auth.dart

--- Flutter_FirebaseAppTutorial/brew_crew/lib/screens/wrapper.dart
+++ Flutter_FirebaseAppTutorial/brew_crew/lib/screens/wrapper.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:brew_crew/screens/authenticate/authenticate.dart';
+// import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:brew_crew/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
       // return either home or authenticate widget
-    return Authenticate();
+    return Home();
   }
 }
