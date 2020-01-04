8330125cc17794443c40ca334f1f0a42d5cdd3fd
dorota <dpawlowska@op.pl>
2020-01-04 20:31:07 +0100

    React Native Tutorial #7 - Flat List Component
    Changes to be committed:
    	modified:   ReactNativeTutorial/myproject/App.js

--- ReactNativeTutorial/myproject/App.js
+++ ReactNativeTutorial/myproject/App.js
@@ -1,5 +1,5 @@
 import React , { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
 
@@ -15,14 +15,25 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+
+      {/* loads only couple items */}
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id} 
+        data={people}
+        renderItem={({ item })=> (
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+      />
       
-      <ScrollView>
+      {/* loads all items at one time */}
+      {/* <ScrollView>
         { people.map(item => (
             <View key={item.id}>
               <Text style={styles.item}>{item.name}</Text>
             </View>
           ))}
-      </ScrollView>
+      </ScrollView> */}
 
     </View>
   );
@@ -42,5 +53,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: 'pink',
     fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   }
 });
