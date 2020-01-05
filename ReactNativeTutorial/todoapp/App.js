5271c8c68f6221c9801dc09bde7bb7444a596af0
dorota <dpawlowska@op.pl>
2020-01-05 17:06:50 +0100

    React Native Tutorial #14 - Flexbox Basics
    Changes to be committed:
    	modified:   ReactNativeTutorial/todoapp/App.js
    	new file:   ReactNativeTutorial/todoapp/components/sandbox.js

--- ReactNativeTutorial/todoapp/App.js
+++ ReactNativeTutorial/todoapp/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyb
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
 
@@ -35,6 +36,8 @@ export default function App() {
   }
 
   return (
+    // <Sandbox />
+
     <TouchableWithoutFeedback onPress={() => {
       Keyboard.dismiss();
       console.log('dismissed keyboard')
@@ -58,7 +61,7 @@ export default function App() {
           </View>
         </View>
       </View>
-    </TouchableWithoutFeedback>
+    </TouchableWithoutFeedback>    
   );
 }
 
@@ -69,9 +72,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
-
+    flex: 1,
   },
   list: {
+    flex: 1,
     marginTop: 20,
   },
 });
