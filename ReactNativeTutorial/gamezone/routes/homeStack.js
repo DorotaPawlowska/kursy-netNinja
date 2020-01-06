b24492cdc70be46a7285686f26f91a525b06a403
dorota <dpawlowska@op.pl>
2020-01-06 20:35:28 +0100

    React Native Tutorial #23 - Navigation Options
    Changes to be committed:
    	modified:   ReactNativeTutorial/gamezone/routes/homeStack.js

--- ReactNativeTutorial/gamezone/routes/homeStack.js
+++ ReactNativeTutorial/gamezone/routes/homeStack.js
@@ -5,15 +5,28 @@ import ReviewDetails from '../screens/reviewDetails';
 
 
 const screens = {
-  Home: {
-    screen: Home
+  GameZone: { // inaczej niż w tutorialu
+    screen: Home,
+    navigatonOptions: {
+      // title: 'GameZone', // to też nie działa
+      headerStyle: { backgroundColor: '#333'} // to też nie działa
+    }
   },
   ReviewDetails: {
-    screen: ReviewDetails
+    screen: ReviewDetails,
+    navigatonOptions: {
+      title: 'Review Details', // to też nie działa
+      headerStyle: { backgroundColor: '#eee'} // to też nie działa
+    }
   },
 
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: '#eee', height: 80 } // to DZIAŁA!!
+  }
+});
 
 export default createAppContainer(HomeStack);
