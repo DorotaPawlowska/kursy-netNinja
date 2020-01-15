ac8d8a9e2c240729577c2e8560b63002cbda7a54
dorota <dpawlowska@op.pl>
2020-01-15 21:25:23 +0100

    React Native Tutorial #25 - Custom Header Component
    Changes to be committed:
    	modified:   ReactNativeTutorial/gamezone02/routes/aboutStack.js
    	modified:   ReactNativeTutorial/gamezone02/routes/homeStack.js
    	new file:   ReactNativeTutorial/gamezone02/shared/header.js

--- ReactNativeTutorial/gamezone02/routes/aboutStack.js
+++ ReactNativeTutorial/gamezone02/routes/aboutStack.js
@@ -1,11 +1,16 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import About from '../screens/about';
+import Header from '../shared/header';
+import React from 'react';
+
 
 const screens = {
   About: {
     screen: About, 
-    navigationOptions: {
-      title: 'About GameZone',
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerTitle: () => <Header navigation={navigation} title='About GameZone'/>,
+      }
     }
   },
 };
@@ -13,7 +18,7 @@ const screens = {
 const AboutStack = createStackNavigator(screens, {
   defaultNavigationOptions: {
     headerTintColor: '#444',
-    headerStyle: { backgroundColor: '#eee', height: 60 },
+    headerStyle: { backgroundColor: '#eee', height: 90 },
   }
 });
 
