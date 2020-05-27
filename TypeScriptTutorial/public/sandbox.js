4c2b9377eb1b454687ec3353db78c26bee60d73c
dorota <dpawlowska@op.pl>
2020-05-27 20:24:11 +0200

    TypeScript Tutorial #8 - Function Basics
    Changes to be committed:
    	modified:   TypeScriptTutorial/public/sandbox.js
    	deleted:    TypeScriptTutorial/public/test.js
    	modified:   TypeScriptTutorial/src/sandbox.ts
    	deleted:    TypeScriptTutorial/src/test.ts

--- TypeScriptTutorial/public/sandbox.js
+++ TypeScriptTutorial/public/sandbox.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('mario');
+var greet;
+// greet = 'hello';
+greet = function () {
+    console.log('hello');
+};
+// const add = ( a: number, b: number, c?: number | string ) => { // optional 3 parameter
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 20);
+var minus = function (a, b) {
+    return a + b;
+};
+var result = minus(10, 7);
+// result = 'something';
--- TypeScriptTutorial/src/test.ts
+++
@@ -1,1 +0,0 @@
-console.log('texting1234');
