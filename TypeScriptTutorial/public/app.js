a0bbd50f559c384caebd6da7dc6627cb9e27db4f
dorota <dpawlowska@op.pl>
2020-05-31 19:30:41 +0200

    TypeScript Tutorial #12 - Classes
    Changes to be committed:
    	modified:   TypeScriptTutorial/public/app.js
    	modified:   TypeScriptTutorial/src/app.ts

--- TypeScriptTutorial/public/app.js
+++ TypeScriptTutorial/public/app.js
@@ -4,6 +4,27 @@
 //   console.log(anchor.href);
 // }
 // console.log(anchor.href);
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// console.log(invOne, invTwo);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 150;
+console.log(invoices);
 // const form = document.querySelector('form')!;
 var form = document.querySelector('.new-item-form');
 // console.log(form.children);
--- TypeScriptTutorial/src/app.ts
+++ TypeScriptTutorial/src/app.ts
@@ -1,10 +1,40 @@
 // const anchor = document.querySelector('a')!;
-
 // if(anchor){
 //   console.log(anchor.href);
 // }
-
 // console.log(anchor.href);
+
+
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format(){
+    return `${this.client} owes $${this.amount} for ${this.details}`
+  }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+// console.log(invOne, invTwo);
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'yoshi';
+invTwo.amount = 150;
+
+console.log(invoices);
+
 
 // const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
