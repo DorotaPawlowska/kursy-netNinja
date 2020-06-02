0533ec616cac9496cdf8ab51b86c4a2fbf01884e
dorota <dpawlowska@op.pl>
2020-06-02 19:37:58 +0200

    TypeScript Tutorial #16 - Interfaces with Classes
    Changes to be committed:
    	modified:   TypeScriptTutorial/public/app.js
    	new file:   TypeScriptTutorial/public/classes/Payment.js
    	new file:   TypeScriptTutorial/public/interfaces/HasFormatter.js
    	modified:   TypeScriptTutorial/src/app.ts
    	modified:   TypeScriptTutorial/src/classes/Invoice.ts
    	new file:   TypeScriptTutorial/src/classes/Payment.ts
    	new file:   TypeScriptTutorial/src/interfaces/HasFormatter.ts

--- TypeScriptTutorial/src/app.ts
+++ TypeScriptTutorial/src/app.ts
@@ -1,56 +1,62 @@
 // interfaces
-interface IsPerson{
-  name: string;
-  age: number;
-  speak(a: string): void;
-  spend(a: number): number;
-}
+// interface IsPerson{
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
 
-const me: IsPerson = {
-  name: 'shaun',
-  age: 30,
-  speak(text: string): void{
-    console.log(text);
-  },
-  spend(amount: number): number{
-    console.log('i spent', amount);
-    return amount;
-  }
-};
+// const me: IsPerson = {
+//   name: 'shaun',
+//   age: 30,
+//   speak(text: string): void{
+//     console.log(text);
+//   },
+//   spend(amount: number): number{
+//     console.log('i spent', amount);
+//     return amount;
+//   }
+// };
 
-// let someone: IsPerson;
-const greetPerson = (person: IsPerson) => {
-  console.log('hello', person.name);
-}
+// // let someone: IsPerson;
+// const greetPerson = (person: IsPerson) => {
+//   console.log('hello', person.name);
+// }
 
-greetPerson(me);
-
-console.log(me);
+// greetPerson(me);
+// console.log(me);
 
 
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice('yoshi', 'web work', 250);
+// docTwo = new Payment('mario', 'plumbing work', 200);
+
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+// console.log(docs);
+
+// const invOne = new Invoice('mario', 'work on the mario website', 250);
+// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
 // console.log(invOne, invTwo);
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
 
-// invOne.client = 'yoshi';
-// invTwo.amount = 150;
-
-// console.log(invoices);
-invoices.forEach(inv => {
-  // inv.client = 'somtehing'; //CANT
-  console.log(inv.client, inv.amount, inv.format());
-});
+// invoices.forEach(inv => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 
 
-// const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type')! as HTMLSelectElement;
@@ -60,10 +66,12 @@ const amount = document.querySelector('#amount')! as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
-  console.log(
-    type.value,
-    tofrom.value,
-    details.value,
-    amount.valueAsNumber
-  );
+
+  let doc: HasFormatter;
+  if(type.value === 'invoice'){
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+  console.log(doc);
 })
