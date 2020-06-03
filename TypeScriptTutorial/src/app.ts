36a154b74f126b63acca9670b49b0f211362789b
dorota <dpawlowska@op.pl>
2020-06-03 19:46:27 +0200

    TypeScript Tutorial #18 - Generics
    Changes to be committed:
      modified:   TypeScriptTutorial/public/app.js
    	modified:   TypeScriptTutorial/src/app.ts

--- TypeScriptTutorial/src/app.ts
+++ TypeScriptTutorial/src/app.ts
@@ -27,4 +27,37 @@ form.addEventListener('submit', (e: Event) => {
   }
 
   list.render(doc, type.value, 'end');
-})
+});
+
+// generics
+
+const addUID = <T extends {name: string}>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+// let docTwo = addUID('hello');
+
+console.log(docOne.age);
+
+//with interfaces
+interface Resource<T>{
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceName: 'preson',
+  data: {name: 'shoun'}
+}
+
+const docFour: Resource<string[]> = {
+  uid: 2,
+  resourceName: 'shoppingList',
+  data: ['data', 'milk']
+}
+
+console.log(docThree, docFour);
