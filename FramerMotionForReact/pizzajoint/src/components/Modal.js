341d9b126bfc2703a470568cf58a0832b8c0177d
dorota <dpawlowska@op.pl>
2020-06-13 15:24:07 +0200

    Framer Motion (for React) #14 - Modal Animation (part 2)
    Changes to be committed:
    	modified:   FramerMotionForReact/pizzajoint/src/App.js
    	modified:   FramerMotionForReact/pizzajoint/src/components/Modal.js
    	modified:   FramerMotionForReact/pizzajoint/src/index.css

--- FramerMotionForReact/pizzajoint/src/components/Modal.js
+++ FramerMotionForReact/pizzajoint/src/components/Modal.js
@@ -7,6 +7,18 @@ const backdropVariants = {
   hidden: { opacity: 0 }
 }
 
+const modalVariants = {
+  hidden: {
+    y: '-100vh',
+    opacity: 0
+  },
+  visible: {
+    y: '200px',
+    opacity: 1,
+    transition: { delay: 0.5 }
+  }
+}
+
 const Modal = ({ showModal, setShowModal }) => {
   return (
     <AnimatePresence exitBeforeEnter>
@@ -14,8 +26,16 @@ const Modal = ({ showModal, setShowModal }) => {
         <motion.div className="backdrop"
         variants={backdropVariants}
         initial="hidden"
-        animate="visible">
-
+        animate="visible"
+        exit="hidden">
+          <motion.div className="modal"
+            variants={modalVariants}
+          >
+            <p>Want to make another pizza?</p>
+            <Link to='/'>
+              <button>start again</button>
+            </Link>
+          </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
