5ad844965a44217d4dbf99cdba7aef47160d5214
dorota <dpawlowska@op.pl>
2020-06-13 16:09:43 +0200

    Framer Motion (for React) #17 - UseCycle Hook
    Changes to be committed:
    	modified:   FramerMotionForReact/pizzajoint/src/components/Loader.js

--- FramerMotionForReact/pizzajoint/src/components/Loader.js
+++ FramerMotionForReact/pizzajoint/src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion, transform } from 'framer-motion';
+import { motion, useCycle } from 'framer-motion';
 
 const loaderVariants = {
   animationOne: {
@@ -16,18 +16,31 @@ const loaderVariants = {
         ease: "easeOut"
       }
     }
+  },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+        ease: "easeOut"
+      }
+    }
   }
 }
 
 const Loader = () => {
+
+  const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
+
   return (
     <>
       <motion.div className="loader"
         variants={loaderVariants}
-        animate="animationOne"
-      >
-
-      </motion.div>
+        animate={animation}
+      ></motion.div>
+      <div onClick={ () => cycleAnimation()}>Cycle Loader</div>
     </>
   )
 }
