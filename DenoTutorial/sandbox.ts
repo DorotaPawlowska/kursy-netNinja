da47e7d355a22f886d526a186220a61719a6e5c3
dorota <dpawlowska@op.pl>
2020-06-15 20:37:33 +0200

    Deno Tutorial #2 - Files, Fetch & Permissions
    Changes to be committed:
    	new file:   readme.txt
    	new file:   sandbox.ts
    	new file:   sandboxFetch.ts
    Changes not staged for commit:
    	modified:   ../README.md

---
+++ DenoTutorial/sandbox.ts
@@ -0,0 +1,19 @@
+// reading files
+// const decoder = new TextDecoder('utf-8');
+
+// const data = await Deno.readFile('readme.txt');
+// console.log(decoder.decode(data));
+
+const data = await Deno.readTextFile('readme.txt');
+console.log(data);
+
+// writing files
+// const encoder = new TextEncoder()
+// const text = encoder.encode('hello again, ninjas');
+
+// await Deno.writeFile('readme.txt', text);
+
+// renamin and removing files
+
+// await Deno.rename('readme.txt', 'deleteme.txt');
+// await Deno.remove('readme.txt');
