246905e312707ea39809fe84f2bdc2cd826df91e
dorota <dpawlowska@op.pl>
2021-01-15 15:15:48 +0100

    Asynchronous JavaScript Tutorial #7 - Promises
    Changes to be committed:
    	modified:   Asynchronous JavaScript Tutorial/chapter_12/sandbox.js

--- Asynchronous JavaScript Tutorial/chapter_12/sandbox.js
+++ Asynchronous JavaScript Tutorial/chapter_12/sandbox.js
@@ -1,36 +1,48 @@
+const getTodos = (resource) => {
 
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState);
-    if(request.readyState === 4 && request.status === 200){
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    }else if(request.readyState === 4){
-      callback('could not fetch data', undefined);
-    }
-  });
-  
-  // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-  // request.open('GET', 'todos.json');
-  request.open('GET', resource);
-  request.send();
+    request.addEventListener('readystatechange', () => {
+      // console.log(request, request.readyState);
+      if(request.readyState === 4 && request.status === 200){
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      }else if(request.readyState === 4){
+        reject('error getting resource');
+      }
+    });
+    
+    request.open('GET', resource);
+    request.send();
+  });  
 };
 
-console.log(1)
-console.log(2);
-
-getTodos('todos/luigi.json', (err, data) => {
-  console.log(data);
-  getTodos('todos/mario.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/shaun.json', (err, data) => {
-      console.log(data);
-    })
-  })
+getTodos('todos/luigi.json').then(data => {
+  console.log('promise resolve:', data);
+}).catch(err => {
+  console.log('promise rejected:', err);
 });
 
-console.log(3);
-console.log(4);
+// promise example
 
+// const getSomething = () => {
+
+//   return new Promise((resolve, reject) => {
+//     // fetch something
+//     // resolve('some data');
+//     reject('some error');
+//   });
+// };
+
+// getSomething().then((data) => {
+//   console.log(data);
+// }, (err) => {
+//   console.log(err)
+// });
+
+// getSomething().then(data => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log(err);
+// });
