6c90e60577afc9b645ee81b85c0661998e70637d
dorota <dpawlowska@op.pl>
2021-01-20 14:41:58 +0100

    Full React Tutorial #5 - Multiple Components
    Changes to be committed:
    	modified:   FullModernReactTutorial/dojo-blog/.eslintcache
    	modified:   FullModernReactTutorial/dojo-blog/src/App.js
    	new file:   FullModernReactTutorial/dojo-blog/src/Home.js
    	new file:   FullModernReactTutorial/dojo-blog/src/Navbar.js

--- FullModernReactTutorial/dojo-blog/src/App.js
+++ FullModernReactTutorial/dojo-blog/src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = { name: 'yoshi', age: 30 }; ERROR! + boolenas
-  const link = "http://www.google.com";
 
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>App Component { title }</h1>
-        <p>liked { likes } times</p>
-        {/* <p>name { person } </p> */}
-        <p>{ 10 }</p>
-        <p>{ "hello ninjas" }</p>
-        <p>{ [1,2,3,4] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href={ link }>google</a>
+        <Home />
       </div>
     </div>
   );
