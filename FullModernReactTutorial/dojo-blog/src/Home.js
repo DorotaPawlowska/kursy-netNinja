35fcc4fb221bb1165b3465d316883043b21f6e00
dorota <dpawlowska@op.pl>
2021-01-25 11:41:38 +0100

    Full React Tutorial #18 - Conditional Loading Message
    Changes to be committed:
    	modified:   FullModernReactTutorial/dojo-blog/.eslintcache
    	modified:   FullModernReactTutorial/dojo-blog/src/Home.js

--- FullModernReactTutorial/dojo-blog/src/Home.js
+++ FullModernReactTutorial/dojo-blog/src/Home.js
@@ -3,20 +3,24 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [ blogs, setBlogs ] = useState(null);
+  const [ isPending, setIsPending ] = useState(true);
 
   useEffect(() => {
+    setTimeout(() => {
       fetch('http://localhost:8000/blogs')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          console.log(data);
-          setBlogs(data);
-        });
+          .then(res => {
+            return res.json();
+          })
+          .then(data => {
+            setBlogs(data);
+            setIsPending(false);
+          });
+    }, 1000);  
   }, []);
 
   return (
     <div className="home">
+      { isPending && <div>loading ...</div> }
       { blogs && <BlogList blogs={blogs} title="all blogs"/> }
     </div>
   );
