3fdecb67b8bee7d729a22cd35355041f974f0f0a
dorota <dpawlowska@op.pl>
2021-01-28 20:19:51 +0100

    Full React Tutorial #29 - Making a POST Request
    Changes to be committed:
    	modified:   FullModernReactTutorial/dojo-blog/.eslintcache
    	modified:   FullModernReactTutorial/dojo-blog/data/db.json
    	modified:   FullModernReactTutorial/dojo-blog/src/Create.js

--- FullModernReactTutorial/dojo-blog/src/Create.js
+++ FullModernReactTutorial/dojo-blog/src/Create.js
@@ -4,12 +4,22 @@ const Create = () => {
   const [ title, setTitle ] = useState('');
   const [ body, setBody ] = useState('');
   const [ author, setAuthor ] = useState('mario');
+  const [ isPending, setIsPending ] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
 
-    console.log(blog);
+    setIsPending(true);
+    
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    })
   }
 
   return ( 
@@ -37,10 +47,8 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>add blog</button>
-        <p>{ title }</p>
-        <p>{ body }</p>
-        <p>{ author }</p>
+        { !isPending && <button>add blog</button> }
+        { isPending && <button disabled>adding blog ...</button> }
       </form>
     </div>
   );
