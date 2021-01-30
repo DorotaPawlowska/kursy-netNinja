0c0b93568faa3a1d1227b8bb64f349bee6735d1d
dorota <dpawlowska@op.pl>
2021-01-30 23:40:52 +0100

    React Query Tutorial #2 - The useQuery Hook
    Changes to be committed:
    	modified:   ReactQueryTutorial/starwars/.eslintcache
    	modified:   ReactQueryTutorial/starwars/src/App.js
    	modified:   ReactQueryTutorial/starwars/src/components/People.js
    	new file:   ReactQueryTutorial/starwars/src/components/Person.js
    	new file:   ReactQueryTutorial/starwars/src/components/Planet.js
    	modified:   ReactQueryTutorial/starwars/src/components/Planets.js
    	modified:   ReactQueryTutorial/starwars/src/index.css

--- ReactQueryTutorial/starwars/src/App.js
+++ ReactQueryTutorial/starwars/src/App.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Navbar from "./components/Navbar";
 import Planets from './components/Planets';
 import People from './components/People';
+
+const queryClient = new QueryClient();
 
 function App() {
   const [ page, setPage] = useState('planets');
@@ -11,7 +14,9 @@ function App() {
       <h1>Star Wars Info</h1>
         <Navbar setPage={setPage}/>
       <div className="content">
-        { page === 'planets' ? <Planets /> : <People /> }
+        <QueryClientProvider client={queryClient}>
+          { page === 'planets' ? <Planets /> : <People /> }
+        </QueryClientProvider>
       </div>
     </div>
   );
