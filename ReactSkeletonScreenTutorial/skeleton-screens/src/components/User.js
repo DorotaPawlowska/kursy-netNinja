5c87425b1f8e635da39ccda531d7fdba1c91e77d
dorota <dpawlowska@op.pl>
2021-02-06 20:02:10 +0100

    React Skeleton Screen Tutorial #5 - Skeleton Profile
    Changes to be committed:
    	modified:   ReactSkeletonScreenTutorial/skeleton-screens/.eslintcache
    	modified:   ReactSkeletonScreenTutorial/skeleton-screens/src/components/User.js
    	modified:   ReactSkeletonScreenTutorial/skeleton-screens/src/skeletons/Skeleton.css
    	new file:   ReactSkeletonScreenTutorial/skeleton-screens/src/skeletons/SkeletonProfile.js

--- ReactSkeletonScreenTutorial/skeleton-screens/src/components/User.js
+++ ReactSkeletonScreenTutorial/skeleton-screens/src/components/User.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import SkeletonProfile from '../skeletons/SkeletonProfile';
 
 const User = () => {
   const [ profile, setProfile ] = useState(null);
@@ -22,7 +23,7 @@ const User = () => {
           <a href={profile.website}>{ profile.website }</a>
         </div>
       )}
-      {!profile && <div>loading...</div> }
+      {!profile && <SkeletonProfile /> }
     </div>
 
   );
